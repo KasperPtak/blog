a7d9724c7d0050aa54e46eca59d608dfcdd6b10d
KasperPtak <kasperptak2@gmail.com>
2024-04-08 17:16:54 +0200

    New homepage
    Created new homepage, confirming laravel was initialized correctly

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// alt way of calling view.
+// Route::view('/','homepage');
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
